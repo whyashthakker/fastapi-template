fc28b600d76fb04d589403e2ff657a30a908d41a
whyashthakker <nynansnh@gmail.com>
2023-08-29 10:10:36 +0530

    safe process friendly error

--- utils/safeprocess.py
+++ utils/safeprocess.py
@@ -10,10 +10,21 @@ def safe_process(func):
             return func(*args, **kwargs)
         except Exception as e:
             function_name = func.__name__
-            error_msg = str(e)
-            logging.error(f"Error occurred in function {function_name}: {error_msg}")
-            if "webhook" not in error_msg.lower():
-                send_failure_webhook(error_msg)
+
+            # Check for specific known errors and replace with user-friendly message
+            if str(e) == "nonsilent_ranges is None or empty.":
+                friendly_error = "The video does not contain any detectable audio."
+            else:
+                friendly_error = "Something went wrong."
+
+            logging.error(
+                f"Error in function {function_name}: {str(e)}"
+            )  # Log the technical error for debugging
+
+            # Send a failure webhook if the error is not related to the webhook itself
+            if "webhook" not in str(e).lower():
+                send_failure_webhook(friendly_error)
+
             raise
 
     return wrapper
